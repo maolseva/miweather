a34dbd537ccda2c838953cc0a134968a8166ab77
maolseva <msepulvedv@gmail.com>
2025-04-22 15:47:18 +0800

    added the search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-function handleSearchSubmit(event) {
-event.preventDefault(); 
-let searchInput = document.querySelector("#search-form-input") ;
-let cityElement = document.querySelector("city");
-cityElement.innerHTML = searchInput.value;
-}
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+  
+    let temperature = response.data.temperature.current;
+    
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+  }
+  
+  function searchCity(city) {
+    const apiKey = "20ac4e1fof3t6e4bfd071a3613757bcc";
+    const apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(refreshWeather);
+  }
+  
+  function handleSearchSubmit(event) {
+    event.preventDefault(); 
+    let searchInput = document.querySelector("#search-form-input");
+    searchCity(searchInput.value);
+  }
+  
+  let form = document.querySelector("#search-form");
+  form.addEventListener("submit", handleSearchSubmit);
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", handleSearchSubmit);
-
+  searchCity("Perth")
